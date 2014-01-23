580a9da36131c8b8b161a50a64bdf086495dc6ec
H. Cole Wiley <hcwiley@gmail.com>
2014-01-23 16:02:25 -0600

    hookay. my create script should be working. lets test it out

---
+++ update_from_live.sh
@@ -0,0 +1,10 @@
+git pull
+cp -r ./the_front /home/hcwiley/webapps/the_front_django/
+cd /home/hcwiley/webapps/the_front_django/the_front/
+export PYTHONPATH=/home/hcwiley/webapps/the_front_django/lib/python2.7/
+/usr/local/bin/python2.7 manage.py collectstatic --noinput
+cp -r ./collected-static/* /home/hcwiley/webapps/the_front_static/
+/usr/local/bin/python2.7 manage.py migrate
+$HOME/bin/pip install -r requirements.txt
+../apache2/bin/restart
+exit
