d4d10b1504c6571b931aff3916839bf7616bb565
H. Cole Wiley <hcwiley@gmail.com>
2014-01-23 17:54:51 -0600

    thanks again timmy

--- update_from_live.sh
+++ update_from_live.sh
@@ -2,6 +2,7 @@ git pull
 cp -r ./the_front /home/hcwiley/webapps/the_front_django/
 cd /home/hcwiley/webapps/the_front_django/the_front/
 export PYTHONPATH=/home/hcwiley/webapps/the_front_django/lib/python2.7/
+export PATH=$PATH:/home/hcwiley/webapps/timmy_hubot/bin/
 /usr/local/bin/python2.7 manage.py collectstatic --noinput
 cp -r ./collected-static/* /home/hcwiley/webapps/the_front_static/
 source ../.env
