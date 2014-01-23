d7e2c471c222b83897ab349cf3841b90b774b70f
H. Cole Wiley <hcwiley@gmail.com>
2014-01-23 17:01:17 -0600

    source .env

--- update_from_live.sh
+++ update_from_live.sh
@@ -4,6 +4,7 @@ cd /home/hcwiley/webapps/the_front_django/the_front/
 export PYTHONPATH=/home/hcwiley/webapps/the_front_django/lib/python2.7/
 /usr/local/bin/python2.7 manage.py collectstatic --noinput
 cp -r ./collected-static/* /home/hcwiley/webapps/the_front_static/
+source ../.env
 /usr/local/bin/python2.7 manage.py migrate
 $HOME/bin/pip install -r requirements.txt
 ../apache2/bin/restart
