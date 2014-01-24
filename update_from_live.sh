a56c81c33a464c2ab31408ae161f960331d52841
H. Cole Wiley <hcwiley@gmail.com>
2014-01-24 17:48:22 -0600

    upgrade killing it?

--- update_from_live.sh
+++ update_from_live.sh
@@ -3,7 +3,7 @@ cp -r ./the_front /home/hcwiley/webapps/the_front_django/
 cd /home/hcwiley/webapps/the_front_django/the_front/
 export PYTHONPATH=/home/hcwiley/webapps/the_front_django/lib/python2.7/
 export PATH=$PATH:/home/hcwiley/webapps/timmy_hubot/bin/
-$HOME/bin/pip install --upgrade -r requirements.txt
+$HOME/bin/pip install -r requirements.txt
 /usr/local/bin/python2.7 manage.py collectstatic --noinput
 cp -r ./collected-static/* /home/hcwiley/webapps/the_front_static/
 source ../.env
