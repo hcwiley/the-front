330fa8bef84dda040cf28be50261fb955cb1a3d5
H. Cole Wiley <hcwiley@gmail.com>
2014-01-24 11:06:43 -0600

    setting up news artcile

--- the_front/the_front/the_front/models.py
+++ the_front/the_front/the_front/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class News(models.Model):
+  name = models.CharField(max_length=100, blank=False, null=False, default="")
+  text = models.TextField(blank=True, null=True, default="")
+  date = models.DateField(_("Date"), default=datetime.date.today)
+  is_archived = models.BooleanField(default=False)
+
+  def __unicode__(self):
+    return "%s (%s)" % (self.name, self.date)
+
