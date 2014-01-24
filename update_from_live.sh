0e8fa50ed903515a8a964d8a4eb1df5df1023550
H. Cole Wiley <hcwiley@gmail.com>
2014-01-24 13:23:33 -0600

    pip first

--- update_from_live.sh
+++ update_from_live.sh
@@ -3,10 +3,10 @@ cp -r ./the_front /home/hcwiley/webapps/the_front_django/
 cd /home/hcwiley/webapps/the_front_django/the_front/
 export PYTHONPATH=/home/hcwiley/webapps/the_front_django/lib/python2.7/
 export PATH=$PATH:/home/hcwiley/webapps/timmy_hubot/bin/
+$HOME/bin/pip install --upgrade -r requirements.txt
 /usr/local/bin/python2.7 manage.py collectstatic --noinput
 cp -r ./collected-static/* /home/hcwiley/webapps/the_front_static/
 source ../.env
 /usr/local/bin/python2.7 manage.py migrate
-$HOME/bin/pip install --upgrade -r requirements.txt
 ../apache2/bin/restart
 exit
