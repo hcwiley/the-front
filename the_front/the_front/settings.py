4cbd41519e6dbd26945c95ba4263c0684c694412
H. Cole Wiley <hcwiley@gmail.com>
2014-05-18 19:10:10 -0500

    no er

--- the_front/the_front/settings.py
+++ the_front/the_front/settings.py
@@ -95,9 +95,9 @@ if IS_DEV:
   PIPELINE_YUGLIFY_BINARY = '/usr/local/share/npm/bin/yuglify'
   PIPELINE_UGLIFYJS_BINARY = '/usr/local/share/npm/lib/node_modules/yuglify/node_modules/uglify-js/bin/uglifyjs'
 else:
-  PIPELINE_STYLUS_BINARY =  '/home/hcwiley/webapps/front_streamer/node_modules/.bin/stylus'
-  PIPELINE_YUGLIFY_BINARY = '/home/hcwiley/webapps/front_streamer/node_modules/.bin/yuglify'
-  PIPELINE_UGLIFYJS_BINARY = '/home/hcwiley/webapps/front_streamer/lib/node_modules/yuglify/node_modules/uglify-js/bin/uglifyjs'
+  PIPELINE_STYLUS_BINARY =  '/home/hcwiley/webapps/front_stream/node_modules/.bin/stylus'
+  PIPELINE_YUGLIFY_BINARY = '/home/hcwiley/webapps/front_stream/node_modules/.bin/yuglify'
+  PIPELINE_UGLIFYJS_BINARY = '/home/hcwiley/webapps/front_stream/lib/node_modules/yuglify/node_modules/uglify-js/bin/uglifyjs'
 
 PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.yuglify.YuglifyCompressor'
 PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
