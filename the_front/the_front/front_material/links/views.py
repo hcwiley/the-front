f6e6d086ea8ade1105018062fd136a109278f754
H. Cole Wiley <hcwiley@gmail.com>
2014-06-02 18:14:27 -0500

    links dont include press links. thanks ryn!

--- the_front/the_front/front_material/links/views.py
+++ the_front/the_front/front_material/links/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render_to_response, redirect
 #from django.http import Http404
 from django.conf import settings
 from django.core.context_processors import csrf
-from front_material.models import Link
+from front_material.models import Link, PressLink
 from the_front.views import common_args
 
 def home(req):
   args = common_args(req)
-  args['links'] = Link.objects.all()
+  args['links'] = Link.objects.exclude(id__in=PressLink.objects.all())
   return render_to_response("links.jade", args)
 
