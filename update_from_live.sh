47e20182f8ce6032db77ef2370505d534cd82915
H. Cole Wiley <hcwiley@gmail.com>
2014-06-08 22:05:23 +0000

    no paypal

--- update_from_live.sh
+++ update_from_live.sh
@@ -2,7 +2,7 @@ git pull
 cp -r ./the_front /home/hcwiley/webapps/the_front_django/
 cd /home/hcwiley/webapps/the_front_django/the_front/
 export PYTHONPATH=/home/hcwiley/webapps/the_front_django/lib/python2.7/
-export PATH=$PATH:/home/hcwiley/webapps/timmy_hubot/bin/
+export PATH=$PATH:/home/hcwiley/webapps/front_stream/bin/
 source ../.env
 $HOME/bin/pip install -r requirements.txt
 /usr/local/bin/python2.7 manage.py collectstatic --noinput
